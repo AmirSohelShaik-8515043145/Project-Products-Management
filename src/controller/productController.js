451f0df26f8623260edb28fdbbf453fbae2b28e5
AmirSohelShaik-8515043145 <amirsohelsk07@gmail.com>
2022-04-16 12:05:15 +0530

    get product added

---
+++ src/controller/productController.js
@@ -0,0 +1,113 @@
+const validator = require('../validator/validator')
+const aws = require('../aws/aws')
+const productModel = require('../model/productModel')
+const currencySymbol = require("currency-symbol-map")
+
+// Create Product Api --------------------------------------------------
+const createProduct = async (req, res) => {
+    try {
+        let data = req.body;
+        if (Object.keys(data) == 0) { return res.status(400).send({ status: false, msg: "Bad request, No data provided." }) };
+
+        let { title, description, price, currencyId, currencyFormat, isFreeShipping, productImage, style, availableSizes, installments } = data
+
+        if (!validator.isValid(title)) { return res.status(400).send({ status: false, msg: "Title is required" }) }
+        let duplicateTitle = await productModel.findOne({ title: title })
+        if (duplicateTitle) return res.status(400).send({ status: false, msg: `${title} is alraedy in use. Please use another title` })
+
+        if (!validator.isValid(description)) { return res.status(400).send({ status: false, msg: "Description is required" }) }
+        if (!validator.isValid(price)) { return res.status(400).send({ status: false, msg: "Price is required" }) }
+        if (!validator.isValid(currencyId)) { return res.status(400).send({ status: false, msg: "CurrencyId is required" }) }
+        if (currencyId != 'INR') { return res.status(400).send({ status: false, msg: "CurrencyId should be INR" }) }
+        data.currencyFormat = currencySymbol('INR')
+
+        if (isFreeShipping) {
+            if ((isFreeShipping != true)) { return res.status(400).send({ status: false, message: 'isFreeShipping must be a boolean value' }) }
+        }
+
+        let files = req.files;
+        if (Object.keys(files).length == 0) { return res.status(400).send({ status: false, msg: "ProductImage is required" }) }
+        const fileRes = await aws.uploadFile(files[0]);
+        data.productImage = fileRes.Location;
+
+        if (style == 0) { return res.status(400).send({ status: false, msg: "Style cannot be empty" }) }
+        if (!(availableSizes)) { return res.status(400).send({ status: false, msg: "Available Size is required" }) }
+
+        // Validation for availableSizes enum :
+        var arr = ["S", "XS", "M", "X", "L", "XXL", "XL"]
+        if (availableSizes) {
+            let sizeArr = availableSizes.split(",").map(x => x.trim())
+            for (let i = 0; i < sizeArr.length; i++) {
+                if (!(arr.includes(sizeArr[i]))) {
+                    return res.status(400).send({ status: false, message: `availableSizes should be among [${arr}]` })
+                }
+            }
+            data.availableSizes = sizeArr
+        }
+
+        if (installments == 0) { return res.status(400).send({ status: false, msg: "Installment cannot be empty" }) }
+
+        let userCreated = await productModel.create(data);
+        res.status(201).send({ status: true, message: "User created successfully", data: userCreated })
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, msg: error.message })
+    }
+}
+
+
+// Get Product Api -----------------------------------------------------
+const getProducts = async function (req, res) {
+    try {
+        data = req.query
+        let filter = {}
+        filter.isDeleted = false
+
+        const { name, size, priceGreaterThan, priceLessThan } = data
+
+        if (name != null) {
+            if (!validator.isValid(name)) { return res.status(400).send({ status: false, msg: "Please provide a Name" }) }
+            filter.title = name
+        }
+        
+        if (size != null) {
+            let arr = ["S", "XS", "M", "X", "L", "XXL", "XL"]
+            if (!arr.includes(size)) { return res.status(400).send({ status: false, msg: "Please select a size in between " + arr }) }
+            filter.availableSizes = size
+        }
+
+        if (priceGreaterThan != null) {
+            console.log(Number(priceGreaterThan))
+            if (Number(priceGreaterThan) == NaN) { return res.status(400).send({ status: false, msg: "Price should be a number" }) }
+            if (Number(priceGreaterThan) <= 0) { return res.status(400).send({ status: false, msg: "Price provide a valid price" }) }
+            filter.price = { $gte: Number(priceGreaterThan) }
+        }
+
+        if (priceLessThan != null) {
+            if (Number(priceLessThan) == NaN) { return res.status(400).send({ status: false, msg: "Price should be a number" }) }
+            if (Number(priceGreaterThan) <= 0) { return res.status(400).send({ status: false, msg: "Price provide a valid price" }) }
+            filter.price = { $lte: Number(priceLessThan) }
+        }
+
+        if (priceGreaterThan != null && priceLessThan != null) {
+            if ((Number(priceGreaterThan) == NaN) || (Number(priceLessThan) == NaN)) { return res.status(400).send({ status: false, msg: ".....Price should be a number" }) }
+            if ((Number(priceGreaterThan) <= 0) || (Number(priceLessThan) <= 0)) { return res.status(400).send({ status: false, msg: ".....Price provide a valid price" }) }
+            filter.price = { $gte: Number(priceGreaterThan), $lte: Number(priceLessThan) }
+        }
+        console.log(filter)
+
+        let product = await productModel.find(filter);
+        if (product.length == 0) return res.status(404).send({ status: false, message: "No product found according to your search" })
+        return res.status(200).send({ status: true, message: 'Products found', data: product })
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+}
+
+
+
+module.exports = {
+    createProduct,
+    getProducts
+}
--- src/route/route.js
+++ src/route/route.js
@@ -2,12 +2,17 @@ const express=require("express")
 const router=express.Router()
 const userController=require("../controller/userController")
 const loginController = require('../controller/loginController')
+const productController = require('../controller/productController')
 
 
 router.post('/register',userController.createUser)
-router.post('/login',loginController.login)
-
 router.get('/user/:userId/profile',userController.getUserProfile)
 router.put('/user/:userId/profile',userController.updateUser)
+
+router.post('/login',loginController.login)
+
+router.post('/products',productController.createProduct)
+router.get('/products',productController.getProducts)
+
 
 module.exports=router
