e44be735a035286ad3c358fa6683c2c6193266bd
AmirSohelShaik-8515043145 <amirsohelsk07@gmail.com>
2022-04-20 04:49:18 +0530

    Complete Project

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -6,9 +6,9 @@ const authentication = async function (req, res, next) {
         let token = req.header('Authorization', 'Bearer Token');
         if (!token) return res.status(400).send({ status: false, msg: "login is required, Set an auth" })
 
-        let splitToken = token.split(" ")[1]
+        let splitToken = token.split(" ")
 
-        let verifiedtoken = jwt.verify(splitToken, "Group-19")
+        let verifiedtoken = jwt.verify(splitToken[1], "Group-19")
         if (!verifiedtoken) return res.status(400).send({ status: false, msg: "token is invalid" })
         next();
     }
@@ -20,8 +20,8 @@ const authentication = async function (req, res, next) {
 let authorization = async function (req, res, next) {
     try {
         let token = req.header('Authorization', 'Bearer Token');
-        let splitToken = token.split(" ")[1]
-        let decodedtoken = jwt.verify(splitToken, "Group-19")
+        let splitToken = token.split(" ")
+        let decodedtoken = jwt.verify(splitToken[1], "Group-19")
         let userId = req.params.userId;
         if (!(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/.test(userId.trim()))) { return res.status(400).send({ status: false, message: "You should have put correct user Id inside params" }) }
 
